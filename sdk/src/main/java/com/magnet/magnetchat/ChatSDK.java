88cfb9b5abfcce9785ac31769596d4a6fbb0dd11
Jim Liu <jim.liu@magnet.com>
2016-03-03 21:48:10 -0800

    rename and make it static

--- sdk/src/main/java/com/magnet/magnetchat/ChatSDK.java
+++ sdk/src/main/java/com/magnet/magnetchat/ChatSDK.java
@@ -1,4 +1,4 @@
-package com.magnet.magnetchat.core.managers;
+package com.magnet.magnetchat;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -6,77 +6,46 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
-import com.magnet.magnetchat.R;
-import com.magnet.magnetchat.helpers.ChannelHelper;
+import com.magnet.magnetchat.core.managers.ChannelCacheManager;
 import com.magnet.magnetchat.helpers.UserHelper;
 import com.magnet.magnetchat.util.Logger;
 import com.magnet.max.android.Max;
 import com.magnet.max.android.User;
-import com.magnet.max.android.config.MaxAndroidPropertiesConfig;
 import com.magnet.mmx.client.api.MMX;
 import com.magnet.mmx.client.api.MMXChannel;
 import com.magnet.mmx.client.api.MMXMessage;
-import java.lang.ref.WeakReference;
 
 /**
  * Created by dlernatovich on 2/8/16.
  * Class which provide to management of the MMX functional
  */
-public class MMXManager {
+public class ChatSDK {
 
-    private static MMXManager instance;
-
-    private final WeakReference<Context> applicationReference;
-
-    private Notification notification;
-
-
-    private MMXManager(Context context) {
+    public static void init(Context context) {
         MMX.registerListener(eventListener);
         MMX.registerWakeupBroadcast(context, new Intent("MMX_WAKEUP_ACTION"));
         com.magnet.mmx.client.common.Log.setLoggable(null, com.magnet.mmx.client.common.Log.VERBOSE);
-
-        //Catch current application as a WeakReference
-        applicationReference = new WeakReference<Context>(context);
     }
 
-    /**
-     * Method which provide the initialization of the instance of the MMXManager
-     * WARNING: Should be initialized only in the Application singleton
-     *
-     * @param context application context
-     * @return
-     */
-    public static MMXManager getInstance(Context context) {
-        if (instance == null) {
-            instance = new MMXManager(context);
-        }
-        return instance;
-    }
-
-    public static MMXManager getInstance() {
-        return instance;
-    }
-
-    public void messageNotification(String channelName, String fromUserName) {
-        PendingIntent intent = PendingIntent.getActivity(applicationReference.get(), 0, new Intent(Intent.ACTION_MAIN)
+    public static void messageNotification(String channelName, String fromUserName) {
+        PendingIntent intent = PendingIntent.getActivity(Max.getApplicationContext(), 0, new Intent(Intent.ACTION_MAIN)
                         .addCategory(Intent.CATEGORY_DEFAULT)
                         .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        .setPackage(applicationReference.get().getPackageName()),
+                        .setPackage(Max.getApplicationContext().getPackageName()),
                 PendingIntent.FLAG_UPDATE_CURRENT);
-        notification = new Notification.Builder(getApplicationContext())
+        Notification notification = new Notification.Builder(Max.getApplicationContext())
                 .setAutoCancel(true)
-                .setSmallIcon(getApplicationContext().getApplicationInfo().icon)
+                .setSmallIcon(Max.getApplicationContext().getApplicationInfo().icon)
                 .setContentTitle("New message is available")
                 .setContentInfo(fromUserName)
                 .setContentIntent(intent).build();
-        NotificationManager manager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager manager = (NotificationManager) Max.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         manager.notify(channelName, 12345, notification);
-        Vibrator v = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+        Vibrator v = (Vibrator) Max.getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
         v.vibrate(500);
     }
 
-    private MMX.EventListener eventListener = new MMX.EventListener() {
+    private static MMX.EventListener eventListener = new MMX.EventListener() {
         @Override
         public boolean onLoginRequired(MMX.LoginReason reason) {
             Logger.debug("login required", reason.name());
@@ -111,13 +80,4 @@ public class MMXManager {
             return false;
         }
     };
-
-    /**
-     * Method which provide to getting of the Application context
-     *
-     * @return current application context
-     */
-    private Context getApplicationContext() {
-        return applicationReference.get();
-    }
 }
