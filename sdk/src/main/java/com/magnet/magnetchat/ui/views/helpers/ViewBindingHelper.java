0ced4a8439f05d86528aaad13f38480a935a4a75
Jim Liu <jim.liu@magnet.com>
2016-03-15 16:44:53 -0700

    added javadoc

--- sdk/src/main/java/com/magnet/magnetchat/ui/views/helpers/ViewBindingHelper.java
+++ sdk/src/main/java/com/magnet/magnetchat/ui/views/helpers/ViewBindingHelper.java
@@ -11,8 +11,17 @@ import android.widget.TextView;
 import com.bumptech.glide.DrawableTypeRequest;
 import com.bumptech.glide.Glide;
 
+/**
+ * Helper to bind additional attributes to widgets
+ */
 public class ViewBindingHelper {
 
+  /**
+   * additional attributes for ImageView
+   * @param view
+   * @param url
+   * @param placeHolder
+   */
   @BindingAdapter(value = {"android:src", "placeHolder"}, requireAll = false)
   public static void setImageUrl(ImageView view, String url, @DrawableRes int placeHolder) {
     DrawableTypeRequest drawableTypeRequest = null;
@@ -33,6 +42,11 @@ public class ViewBindingHelper {
     }
   }
 
+  /**
+   * additional attributes to TextView
+   * @param view
+   * @param resourceId
+   */
   @BindingAdapter(value = {"backgroundRes"}, requireAll = false)
   public static void setTextBackgroundResource(TextView view, @DrawableRes int resourceId) {
     view.setBackgroundResource(resourceId);
